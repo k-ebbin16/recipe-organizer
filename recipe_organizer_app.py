0737c0452d6260f26c5b3d3c98280a248d26c146
EBBIN NANA KWAMINA <123391862+k-ebbin16@users.noreply.github.com>
2023-07-20 13:15:10 +0100

    Fixed some add and view functionality bugs

--- recipe_organizer_app.py
+++ recipe_organizer_app.py
@@ -1,3 +1,12 @@
+""""
+
+GROUP A.10
+    Create a recipe organizer application that allows users to
+    add, view, and search for recipes. Implement classes and methods to
+    represent recipes and organize the functionality of the application.
+
+"""
+
 import recipe_organizer_module as recipeMod
 
 organizer = recipeMod.RecipeOrganizer()
@@ -7,7 +16,7 @@ while True:
     What do you want to do?
     1. Add Recipes
     2. View all Recipes
-    3. View a specific Recipe
+    3. View a specific Recipe (view)
     4. Search Recipes
     5. Exit
     """)
@@ -20,16 +29,31 @@ while True:
         if user_response.lower() == "add recipes" or user_response == str(1):
             print("\nProvide Recipe details")
             recipe_name = input("-- Recipe name: ")
-            recipe_ingredients = input("-- Recipe ingredients: ")
+            if recipe_name == "":
+                print("There must be a recipe name!")
+                continue
+
+            recipe_ingredients = input("-- Recipe ingredients (separate with commas ','): ")
+            if recipe_ingredients == "":
+                print("There must be at least one ingredient!")
+                continue
+
             recipe_instructions = input("-- Recipe instructions (end each instruction with ';'): ")
-            recipe_instructions_list = recipe_instructions.split(';')
-            recipe = recipeMod.Recipe(recipe_name, recipe_ingredients, recipe_instructions_list)
-            organizer.add_recipe(recipe)
+            if recipe_instructions == "":
+                print("There must be at least one instruction!")
+                continue
+            else:
+                recipe_instructions_list = recipe_instructions.split(';')
+            if recipe_name != "" and recipe_ingredients != "" and recipe_instructions != "":
+                recipe = recipeMod.Recipe(recipe_name, recipe_ingredients, recipe_instructions_list)
+                organizer.add_recipe(recipe)
+            else:
+                print("The recipe name, recipe ingredients or recipe instructions may have been left empty!")
 
         elif user_response.lower() == "view all recipes" or user_response == str(2):
             organizer.view_recipes()
 
-        elif user_response == "view a specific recipe" or user_response == str(3):
+        elif user_response.lower() in "view a specific recipe" or user_response == str(3):
             if organizer.recipes is None:
                 print("No recipe has been added yet!")
             else:
--- recipe_organizer_module.py
+++ recipe_organizer_module.py
@@ -8,13 +8,13 @@ class Recipe:
     def display_recipe(self, recipe_name):
         if recipe_name == self.recipe_name:
             print(f"\nRecipe: {self.recipe_name.title()}")
-            print(f"Ingredients: {', '.join(self.ingredients_list)}")
+            print(f"Ingredients: {', '.join(self.ingredients_list)}" if self.ingredients_list is not None else None)
             print("<<< Instructions >>>")
             for n, instruction in dict(enumerate(self.instructions, start=1)).items():
-                if instruction.istitle() is False:
-                    print(f"{n}. {instruction.capitalize().strip()}")
-                else:
+                if instruction.istitle():
                     print(f"{n}. {instruction.strip()}")
+                else:
+                    print(f"{n}. {instruction.strip().capitalize()}")
 
 
 class RecipeOrganizer:
@@ -28,9 +28,9 @@ class RecipeOrganizer:
             self.recipe_list.append(recipe)
             self.recipe_name_list.append(recipe.recipe_name)
             self.recipes = dict(enumerate(self.recipe_list, start=1))
-            print(f"Recipe: '{recipe.recipe_name.title()}' successfully added!")
+            print(f"\nRecipe: '{recipe.recipe_name.title()}' successfully added!")
         else:
-            print(f"Recipe: '{recipe.recipe_name.title()}' has already been added!")
+            print(f"\nRecipe: '{recipe.recipe_name.title()}' has already been added!")
 
     def view_recipes(self, recipe_name_to_view=None):
         if recipe_name_to_view is None:
@@ -41,13 +41,15 @@ class RecipeOrganizer:
             except AttributeError:
                 print("No recipe has been added yet!")
         else:
-            recipe_name_to_view = recipe_name_to_view.lower()
             for r in self.recipe_list:
                 if recipe_name_to_view.lower() == r.recipe_name:
                     Recipe.display_recipe(r, r.recipe_name)
+                    break
                 else:
-                    print(f"'{recipe_name_to_view}' not found! \n'{recipe_name_to_view}' may not be in recipe list or "
-                          f"is spelt wrongly!")
+                    continue
+            else:
+                print(f"'{recipe_name_to_view}' not found! \n'{recipe_name_to_view}' may not be in recipe list or "
+                      f"is spelt wrongly!")
 
     def search_recipe(self, keyword):
         global matching_recipe_list
@@ -65,5 +67,3 @@ class RecipeOrganizer:
                 print(f"\n-- {len(matching_recipe)} recipe(s) found!")
                 for n, r in matching_recipe_list:
                     print(f"{n}. {r.title()}")
-
-
